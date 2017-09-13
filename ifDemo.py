7daf6a8de8b0c78e75a71b63c7138e94b825ef41
PedroGal1234 <31485824+PedroGal1234@users.noreply.github.com>
2017-09-13 10:10:23 -0400

    Updated from Brython Server: 9/13/2017 10:10:22 AM

--- ifDemo.py
+++ ifDemo.py
@@ -1,1 +1,11 @@
+#Pedro Gallino
+#9/13/17
+#ifDemo.py - First If statements
 
+num = int(input('Enter a number: '))
+if num>0:
+    print('The number is positive')
+elif num<0:
+    print('The number is negative')
+else:
+    print('The number is zero')
