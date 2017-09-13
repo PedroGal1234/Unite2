a7b04d54ca0af1ebdcf62f3d49486c91c0403f77
PedroGal1234 <31485824+PedroGal1234@users.noreply.github.com>
2017-09-13 13:25:32 -0400

    Updated from Brython Server: 9/13/2017 1:25:31 PM

--- gradeCalculator.py
+++ gradeCalculator.py
@@ -1,1 +1,15 @@
+#Pedro Gallino
+#9/12/17
+# gradeCalculator.py - tells you your letter grade
 
+grade = int(input('Enter your grade: '))
+if grade>=93:
+    print('You earned a A')
+elif grade>=80:
+    print('You earned a B')
+elif grade>=70:
+    print('You earned a C')
+elif grade>=60:
+    print('You earned a D')
+else:
+    print('You earned a NC')
