31cb86cd16f4e88955e3d95d5853825e6d9c6559
PedroGal1234 <31485824+PedroGal1234@users.noreply.github.com>
2017-09-14 14:49:40 -0400

    Updated from Brython Server: 9/14/2017 2:49:29 PM

--- insulter.py
+++ insulter.py
@@ -1,1 +1,19 @@
+#Pedro Gallino
+#9/14/17
+#insulter.py- insults people
 
+from random import randint
+
+name = input('What is your name: ')
+num = randint(1,5)
+
+if num == 1:
+    print('If you look up',name,'In the dictionary, it says the sutpidest: person ever')
+elif num == 2:
+    print('You are a potato')
+elif num == 3:
+    print('Your feet smell')
+elif num == 4:
+    print('You have no friends')
+elif num == 5:
+    print('People like you (sarcasm)')
