278a30c120746af4bd94b3afce2fe650a65322e6
PedroGal1234 <31485824+PedroGal1234@users.noreply.github.com>
2017-09-14 11:27:08 -0400

    Updated from Brython Server: 9/14/2017 11:27:08 AM

--- unitConverter.py
+++ unitConverter.py
@@ -8,8 +8,17 @@ print('3)Liters to Gallons')
 print('4)Celsius to Fahrenheit')
 rate = int(input('Choose a Number:'))
 
-if rate=1:
-    km = float(input('Enter number of kilometers:')
+if rate==1:
+    km = float(input('Enter number of kilometers:'))
     print(km,'Kilometers is',km*0.621371,'miles')
+elif rate==2:
+    kg = float(input('Enter number of kilograms:'))
+    print(kg,'Kilograms is',kg*2.20462,'pounds')
+elif rate==3:
+    liter = float(input('Enter number of Liters:'))
+    print(liter,'Liter is',liter*0.264172,'Gallons')
+elif rate==4:
+    cel = float(input('Enter degrees Celsius:'))
+    print(cel,'degrees Celsius is',(cel*(9/5))+32,'degrees Fahrenheit')
 
 
