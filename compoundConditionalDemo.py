aef3db0564abb4c052e3954a90f200b4e4214c4e
PedroGal1234 <31485824+PedroGal1234@users.noreply.github.com>
2017-09-14 14:22:08 -0400

    Updated from Brython Server: 9/14/2017 2:22:07 PM

--- compoundConditionalDemo.py
+++ compoundConditionalDemo.py
@@ -1,1 +1,16 @@
+#Pedro Gallino
+#9/14/17
+#compoundConditionalDemo.py - using 'and' and 'or'
 
+num = float(input('Enter a number: '))
+
+if num>0 and num%7 == 0:
+    print('Your number is positive and divisible by 7')
+elif num>0:
+    print('Your number is posistive and not divisible by 7')
+elif num<0 and num%7 == 0:
+    print('Your number is negative and divisible by 7')
+elif num<0:
+    print('Your number is negative and not divisible by 7')
+else:
+    print('Your number is zero')
