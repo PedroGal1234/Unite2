64c808d353c80ed1099271c1a23d9cb55e12c9b9
PedroGal1234 <31485824+PedroGal1234@users.noreply.github.com>
2017-09-14 11:14:55 -0400

    Updated from Brython Server: 9/14/2017 11:14:55 AM

--- unitConverter.py
+++ unitConverter.py
@@ -1,1 +1,15 @@
+#Pedro Gallino
+#9/14/17
+#unitConverter.py - converts units
+
+print('1)Kilometers to Miles')
+print('2)Kilograms to Pounds')
+print('3)Liters to Gallons')
+print('4)Celsius to Fahrenheit')
+rate = int(input('Choose a Number:'))
+
+if rate=1:
+    km = float(input('Enter number of kilometers:')
+    print(km,'Kilometers is',km*0.621371,
+
 
