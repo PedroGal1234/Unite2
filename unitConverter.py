cc3f37c368d59a91b4f113266af836e04b90da07
PedroGal1234 <31485824+PedroGal1234@users.noreply.github.com>
2017-09-28 14:43:50 -0400

    Updated from Brython Server: 9/28/2017 2:43:49 PM

--- unitConverter.py
+++ unitConverter.py
@@ -2,23 +2,26 @@
 #9/14/17
 #unitConverter.py - converts units
 
-print('1)Kilometers to Miles')
-print('2)Kilograms to Pounds')
-print('3)Liters to Gallons')
-print('4)Celsius to Fahrenheit')
-rate = int(input('Choose a Number:'))
+while True:
+    print('1)Kilometers to Miles')
+    print('2)Kilograms to Pounds')
+    print('3)Liters to Gallons')
+    print('4)Celsius to Fahrenheit')
+    print('5)Is quit')
+    rate = int(input('Choose a Number:'))
 
-if rate==1:
-    km = float(input('Enter number of kilometers:'))
-    print(km,'Kilometers is',km*0.621371,'miles')
-elif rate==2:
-    kg = float(input('Enter number of kilograms:'))
-    print(kg,'Kilograms is',kg*2.20462,'pounds')
-elif rate==3:
-    liter = float(input('Enter number of Liters:'))
-    print(liter,'Liter is',liter*0.264172,'Gallons')
-elif rate==4:
-    cel = float(input('Enter degrees Celsius:'))
-    print(cel,'degrees Celsius is',(cel*(9/5))+32,'degrees Fahrenheit')
-
+    if rate==1:
+        km = float(input('Enter number of kilometers:'))
+        print(km,'Kilometers is',km*0.621371,'miles')
+    elif rate==2:
+        kg = float(input('Enter number of kilograms:'))
+        print(kg,'Kilograms is',kg*2.20462,'pounds')
+    elif rate==3:
+        liter = float(input('Enter number of Liters:'))
+        print(liter,'Liter is',liter*0.264172,'Gallons')
+    elif rate==4:
+        cel = float(input('Enter degrees Celsius:'))
+        print(cel,'degrees Celsius is',(cel*(9/5))+32,'degrees Fahrenheit')
+    else:
+        break
 
